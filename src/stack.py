f713be2bfe23717179207c86bbbf5776287dd0eb
Sergey <e-box@live.ru>
2023-05-17 21:03:48 +0800

    Filled stack.py, finished test_stack.py

--- src/stack.py
+++ src/stack.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,10 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        # Список узлов стека
+        self.node_list = []
+        # Верхний элемент стека
+        self.top = None
 
     def push(self, data):
         """
@@ -23,7 +27,12 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        # Определяем второй элемент узла стека
+        second_element = None if len(self.node_list) < 1 else self.node_list[-1]
+        # Определяем узел стека на вершине стека
+        self.top = Node(data, second_element)
+        # Добавляем новый узел в список узлов стека
+        self.node_list.append(self.top)
 
     def pop(self):
         """
