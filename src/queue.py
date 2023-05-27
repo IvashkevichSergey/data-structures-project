55e638658ef337dc839dfe6931813693fd4f7957
Sergey <e-box@live.ru>
2023-05-27 21:14:50 +0800

    Homework #3 finished

--- src/queue.py
+++ src/queue.py
@@ -1,7 +1,7 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
@@ -18,9 +18,6 @@ class Queue:
         """Конструктор класса Queue"""
         self.head = None
         self.tail = None
-        self.list_of_data = []
-        self.next_node_1 = None
-        self.next_node_2 = None
 
     def enqueue(self, data):
         """
@@ -28,15 +25,19 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        self.next_node_2 = self.next_node_1
-        self.next_node_1 = self.list_of_data[-1] if self.list_of_data else None
-        self.list_of_data.append(data)
-        self.tail = Node(data, None)
-        self.head = Node(self.list_of_data[0], Node(self.next_node_1, self.next_node_2))
+        node = Node(data)
+        # Если очередь пустая - задаем одно и то же значение и для
+        # начала и для конца очереди
+        if not self.head:
+            self.head = self.tail = node
+        else:
+            self.tail.next_node = node
+            self.tail = node
 
     def dequeue(self):
         """
-        Метод для удаления элемента из очереди. Возвращает данные удаленного элемента
+        Метод для удаления элемента из очереди.
+        Возвращает данные удаленного элемента
 
         :return: данные удаленного элемента
         """
@@ -44,4 +45,14 @@ class Queue:
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        return '\n'.join(self.list_of_data)
+        # Если данных в очереди нет - возвращает пустую строку
+        if not self.head:
+            return ''
+        tmp_var = self.head
+        list_of_data = [self.head.data]
+        # Добавляем в список имеющиеся в очереди данные до тех пор,
+        # пока next_node не будет иметь значение None
+        while tmp_var.next_node:
+            tmp_var = tmp_var.next_node
+            list_of_data.append(tmp_var.data)
+        return '\n'.join(list_of_data)
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,26 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src.queue import Queue, Node
+
+
+class TestNode(unittest.TestCase):
+
+    def test_node(self):
+        self.assertEqual(Node('str', 'abc').data, 'str')
+        self.assertEqual(Node('str', 'abc').next_node, 'abc')
+
+
+class TestQueue(unittest.TestCase):
+    def test_queue(self):
+        queue = Queue()
+        queue.enqueue('first')
+        self.assertEqual(queue.enqueue('second'), None)
+        self.assertIsInstance(queue.head, Node)
+        self.assertIsInstance(queue.tail, Node)
+        self.assertEqual(queue.head.data, 'first')
+        self.assertEqual(queue.tail.data, 'second')
+        self.assertEqual(str(queue), 'first\nsecond')
+        queue.enqueue('third')
+        self.assertEqual(queue.head.next_node.data, 'second')
+        self.assertEqual(queue.tail.next_node, None)
+
